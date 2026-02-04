be92c4e5822e9def13dcb065f46bf018ef0c05f5
saifshah00 <94551347+saifshah00@users.noreply.github.com>
2026-02-04 18:52:20 +0000

    sqlalchemy & Pymysql
    .

--- database.py
+++ database.py
@@ -4,13 +4,24 @@ from sqlalchemy import create_engine, text
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 engine = create_engine(db_connection_string)
 
-def load_job_from_db(id):
+def load_jobs_from_db():
     with engine.connect() as conn:
         result = conn.execute(
-            text("SELECT * FROM jobs WHERE id = :val"), {"val": id}
-        )
+            text("SELECT * FROM jobs"))
+        jobs = []
+        for row in result.all():
+            jobs.append(dict(row._mapping))
+        return  jobs
+
+
+def load_job_from_db(id):
+   with engine.connect() as conn:
+        result = conn.execute(
+            text("SELECT * FROM jobs WHERE id = :val"), {"val": id})
         rows = result.all()
         if len(rows) == 0:
-            return None
+           return None
         else:
-            return dict(rows[0]._mapping)
+           return dict(rows[0]._mapping)
+
+    
