66de2d1c4c86d1040339a6f38e77c37bc5c3fc75
saifshah00 <94551347+saifshah00@users.noreply.github.com>
2026-02-21 16:14:12 +0000

    update database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import engine, load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import engine, load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_copycat():
     jobs_list = load_jobs_from_db()
-    return render_template('home.html', company_name='copycat', jobs=jobs_list)
+    return render_template('home.html', jobs=jobs_list)
 
 
 @app.route("/api/jobs")
@@ -18,8 +18,22 @@ def list_jobs():
 @app.route("/job/<id>")
 def show_job(id):
     job = load_job_from_db(id)
-    return jsonify(job)
+    if not job:
+         return "Not Found", 404
+    return render_template('jobpage.html', 
+                           job= job)
 
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+    data = request.form
+    job = load_job_from_db(id)
+    add_application_to_db(id, data)
+    # store this in the DB
+    # send an email
+    # display an acknowledgement
+    return render_template('application_submitted.html',
+                            application=data,
+                            job=job)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -4,24 +4,44 @@ from sqlalchemy import create_engine, text
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 engine = create_engine(db_connection_string)
 
+
 def load_jobs_from_db():
     with engine.connect() as conn:
-        result = conn.execute(
-            text("SELECT * FROM jobs"))
+        result = conn.execute(text("SELECT * FROM jobs"))
         jobs = []
         for row in result.all():
             jobs.append(dict(row._mapping))
-        return  jobs
+        return jobs
 
 
 def load_job_from_db(id):
-   with engine.connect() as conn:
-        result = conn.execute(
-            text("SELECT * FROM jobs WHERE id = :val"), {"val": id})
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),
+                              {"val": id})
         rows = result.all()
         if len(rows) == 0:
-           return None
+            return None
         else:
-           return dict(rows[0]._mapping)
+            return dict(rows[0]._mapping)
 
-    
+
+def add_application_to_db(job_id, data):
+    with engine.connect() as conn:
+        query = text("""
+            INSERT INTO applications (
+                job_id, full_name, email, linkedin_url, education, work_experience, resume_url
+            ) VALUES (
+                :job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url
+            )
+        """)
+
+        conn.execute(
+            query, {
+                "job_id": job_id,
+                "full_name": data["full_name"],
+                "email": data["email"],
+                "linkedin_url": data["linkedin_url"],
+                "education": data["education"],
+                "work_experience": data["work_experience"],
+                "resume_url": data["resume_url"]
+            })
